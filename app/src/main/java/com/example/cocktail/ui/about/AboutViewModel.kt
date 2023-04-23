ff64f19d3a5ae9c41f82d0a737021a0e0c4577e7
Bence Valent <vabe97@gmail.com>
2023-04-23 18:30:48 +0200

    style(about): update mutable text

--- app/src/main/java/com/example/cocktail/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/example/cocktail/ui/about/AboutViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "About fragment"
     }
     val text: LiveData<String> = _text
 }
