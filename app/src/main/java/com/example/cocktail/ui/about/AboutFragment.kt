5be1708530df4303486181dced79db47ee1e53fc
Bence Valent <vabe97@gmail.com>
2023-05-26 12:35:31 +0200

    feat: implement crashlytics

--- app/src/main/java/com/example/cocktail/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/cocktail/ui/about/AboutFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -30,6 +31,10 @@ class AboutFragment : Fragment() {
 
         context?.packageManager?.getPackageInfo(requireContext().packageName, 0)?.let {
             binding.aboutVersion.text = "Current version: ${it.versionName}"
+        }
+
+        binding.appLogoCrash.setOnClickListener {
+            throw RuntimeException("Test Crash") // Force a crash
         }
 
         return binding.root
